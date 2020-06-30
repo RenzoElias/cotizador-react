ea0d358376d6a3f877faa7370bc8ee368f41a2ad
RenzoElias <57267514+RenzoElias@users.noreply.github.com>
2020-06-30 00:31:58 -0500

    Updates

---
+++ precache-manifest.5c2cb21b5b943969107cbfd7c25fc41d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a60a591aa7790d7e3627ce53f37c814",
+    "url": "/cotizador-react/index.html"
+  },
+  {
+    "revision": "2998287282ca0a02942e",
+    "url": "/cotizador-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6dbcbc77dc82126d92d6",
+    "url": "/cotizador-react/static/js/2.272932f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cotizador-react/static/js/2.272932f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2998287282ca0a02942e",
+    "url": "/cotizador-react/static/js/main.6325e1b5.chunk.js"
+  },
+  {
+    "revision": "5e3fce2df6820f78cd75",
+    "url": "/cotizador-react/static/js/runtime-main.088b7bd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cotizador-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
