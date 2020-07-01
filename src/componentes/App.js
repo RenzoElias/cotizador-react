c2bc453151757dee3408955b5479817a49bc59bc
RenzoElias <57267514+RenzoElias@users.noreply.github.com>
2020-06-30 20:53:08 -0500

    Componente limpio y con los scripts deploy Git Page

---
+++ src/componentes/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="contenedor">
+        <p>
+          Desde el componente limpio y con los scripts deploy
+        </p>
+    </div>
+  );
+}
+
+export default App;
