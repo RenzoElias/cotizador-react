b2d1204004e01de43cbad3706e9cf4d021bf70dc
RenzoElias <57267514+RenzoElias@users.noreply.github.com>
2020-07-07 22:26:46 -0500

    Se integra las funciones para calcular precio

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, {Component} from "react";
+import Header from "./Header";
+import Formulario from "./Formulario";
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
-function App() {
-  return (
-    <div className="contenedor">
-        <p>
-          Desde el componente limpio y con los scripts deploy
-        </p>
-    </div>
-  );
+class App extends Component {
+    cotizarSeguro = (datos) => {
+        //console.log(datos);
+        const { marca, plan, year} = datos
+
+        //-agregar una base de 2000
+        let resultado = 2000;
+
+        //-Obtener la diferencia de años
+        const diferencia = obtenerDiferenciaAnio(year)
+
+        //console.log('La diferencia es '+ diferencia)
+
+        //-por cada año restar el 3% al valor del seguro
+        resultado -= ((diferencia*3)*resultado) / 100;
+        // console.log(resultado)
+
+        //-Americano 15%, asiatico 5% y europeo 30% de incremento al valor actual
+        resultado = calcularMarca(marca) * resultado
+        // console.log(resultado)
+
+        //-el plan del auto, el basico incrementa el valor 20%, cobertura completa 50%
+        let incrementoPlan = obtenerPlan(plan)
+        // console.log(incrementoPlan)
+
+        //-dependiendo del plan incrementar
+        resultado = parseFloat(incrementoPlan * resultado).toFixed(2); 
+
+        //-ya tenemos el costo
+        console.log(resultado)
+    };
+    render() {
+        return (
+            <div className="contenedor">
+                <Header titulo="Cotizador de Seguro de Auto" />
+
+                <div className="contenedor-formulario">
+                    <Formulario cotizarSeguro={this.cotizarSeguro} />
+                </div>
+                
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/componentes/Formulario.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+
+class Formulario extends Component {
+    //refs son para leer los valores de los campos(que ingresa el usuario) de un formulario
+    //Un ref por cada campo
+    marcaRef = React.createRef();
+    yearRef = React.createRef();
+    planBasicoRef = React.createRef();
+    planCompletoRef = React.createRef();
+    //ambos son lo mismo solo que uno ocupa mas codigo que el otro ↑↓
+    // constructor(props) {
+    //     super(props);
+    //     this.marcaRef = React.createRef();
+    //     this.yearRef = React.createRef();
+    //     this.planBasicoRef = React.createRef();
+    //     this.planCompletoRef = React.createRef();
+    // }
+
+    //si no se pone el "e" sale en link http://localhost:3000/cotizador-react?marca=americano&year=2018 // con los campos que tienes agregado // para evitar eso se pone el e
+    // cotizarSeguro(e){
+    cotizarSeguro = (e) => {
+        //con esto se previene el comportamiento por default
+        e.preventDefault();
+        // console.log('Enviado..!');
+        // console.log(this.marcaRef);
+        // console.log(this);
+        //el this no esta apuntando correctamente(por el tema de la herencia extends de la clase), para solucionarlo se agrega en .bind(this) para que apunte que es de esta clase correctamente
+
+        
+        // Obtener los datos
+        // console.log(this.marcaRef.current.value);
+            // operador condicional ternario JS // https://www.w3schools.com/js/js_comparisons.asp
+            // JavaScript también contiene un operador condicional que asigna un valor a una variable en función de alguna condición.
+            // variablename = (condition) ? value1:value2 
+            // variablename = (true o false) ?  retornara esto Si es true : retornara esto Si es false      
+        const plan = this.planBasicoRef.current.checked ? 'basico' : 'completo'
+
+        // Crear el objeto
+        const infoAuto = {
+            marca: this.marcaRef.current.value,
+            year: this.yearRef.current.value,
+            plan: plan
+        }
+
+        // console.log(infoAuto);
+
+        // Enviarlo al componente principal
+        this.props.cotizarSeguro(infoAuto)
+
+        // Opcional
+        // Resetear el formulario (para que se vuelva a renderizar)
+        // console.log(e.currentTarget)
+        // e.currentTarget.reset();
+
+    };
+
+    render() {
+        return (
+            // action="enviar.php" // la forma clasica // pero en react es ↓↓ // con un metodo (cotizarSeguro)
+            //La segunda manera mas facil es haciendo ese metodo arrow function y no habra problema con la referencia del this
+            // <form className="cotizar-auto" onSubmit={this.cotizarSeguro.bind(this)}>
+            <form className="cotizar-auto" onSubmit={this.cotizarSeguro}>
+                <div className="campo">
+                    <label>Marca</label>
+                    {/* Se integra el ref */}
+                    <select name="marca" ref={this.marcaRef}>
+                        <option value="americano">Americano</option>
+                        <option value="europeo">Europeo</option>
+                        <option value="asiatico">Asiatico</option>
+                    </select>
+                </div>
+
+                <div className="campo">
+                    <label>Año</label>
+                    <select name="year" ref={this.yearRef}>
+                        <option value="2018">2018</option>
+                        <option value="2017">2017</option>
+                        <option value="2016">2016</option>
+                        <option value="2015">2015</option>
+                        <option value="2014">2014</option>
+                        <option value="2013">2013</option>
+                        <option value="2012">2012</option>
+                        <option value="2011">2011</option>
+                        <option value="2010">2010</option>
+                        <option value="2009">2009</option>
+                        <option value="2008">2008</option>
+                    </select>
+                </div>
+                <div className="campo">
+                    <label>Plan:</label>
+                    <input
+                        type="radio"
+                        ref={this.planBasicoRef}
+                        name="plan"
+                        value="basico"
+                    />{" "}
+                    Básico
+                    <input
+                        type="radio"
+                        ref={this.planCompletoRef}
+                        name="plan"
+                        value="completo"
+                    />{" "}
+                    Completo
+                </div>
+                {/* Etiquetas que tengan su propio cierre, es decir, ejemplo: <label></label> Normal React lo toma
+                Pero si es una etiqueta sin cierre como <input type="radio"> se tendra que poner <input type="radio" /> para que jsx lo pueda leer y no haiga error */}
+                <button type="submit" className="boton">
+                    Cotizar
+                </button>
+            </form>
+        );
+    }
+}
+
+export default Formulario;
